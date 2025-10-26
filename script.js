32f26aa8fe27a32454f478907a104b11ede4b78d
VIMALCHINTU <158822830+VIMALCHINTU@users.noreply.github.com>
2025-10-26 13:47:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 function getFormvalue() {
-  // 1. Retrieve input values
-  let fname = document.getElementById("fname").value.trim();
-  let lname = document.getElementById("lname").value.trim();
-
-  // 2. Concatenate full name
-  let fullName = fname + " " + lname;
-
-  // 3. Handle empty case
-  if (!fname && !lname) {
-    alert("Please enter your first and last name.");
-    return;
-  }
-
-  // 4. Display result
-  alert(fullName);
+	let fname=document.getElementById("fname")
+	let lname=document.getElementById("lname")
+	alert(fname.value+" "+lname.value)
+  
 }
